dee185d3a70ad2e38e684a1b011ba4fab9aff060
Sercan Kalkan <sercankalkan1@gmail.com>
2023-06-16 23:31:45 +0300

    prompt asking for square numbers added

--- main.js
+++ main.js
@@ -6,21 +6,52 @@ let newColor = document.querySelectorAll(".row");
                 color.style.background = 'black';
             });
           }
-
-//ask user the number of squares for both sides
-//document.getElementById("clickMe").onclick = function () { alert('hello!'); };
-document.getElementById('ask').onclick = function () {
-    let squareNumber = Number(prompt("How many squares do you want?"));
-    console.log(typeof(squareNumber));
-     if(squareNumber>100) {
-        alert('You must choose a number smaller than 100')
-     }
-
 */
-let columns =50;
-let rows =50;
+//create user the number of squares for both sides
+
+const button = document.getElementById('create');
+button.addEventListener('click',askSquareNumber);
+button.addEventListener('click',createSketch);
+
+let columns =16;
+let rows =columns;
 const height = `${(640/rows)}px`//define height of square divs
 
+function askSquareNumber() {
+    columns = Number(prompt("How many squares do you want?"));
+    console.log(typeof(columns));
+    if(columns>100) {
+    alert('You must choose a number smaller than 100')
+ }
+const freeSpace = document.getElementById('sketch');
+freeSpace.innerHTML=""
+sketch.inner
+createSketch(columns);
+}
+
+function createSketch (columns) {
+    for (let i = 0; i < columns; ++i) {
+        let column = document.createElement('div'); // create column
+        column.classList.add('column');
+        column.style.setProperty("height", height)
+      
+        for (let j = 0; j < rows; ++j) {
+            let row = document.createElement('div'); // create row
+            row.classList.add('row');
+            row.style.setProperty("height", height)
+            column.appendChild(row); // append row in column
+          
+        }
+        let sketch= document.getElementById('sketch');
+        sketch.appendChild(column); // append column inside grid
+      }
+}
+/*
+
+let columns =25;
+let rows =25;
+
+const height = `${(640/rows)}px`//define height of square divs
 
 for (let i = 0; i < columns; ++i) {
   let column = document.createElement('div'); // create column
@@ -37,7 +68,7 @@ for (let i = 0; i < columns; ++i) {
   let sketch= document.getElementById('sketch');
   sketch.appendChild(column); // append column inside grid
 }
-
+*/
 
 //adding squares to sketch
 /*
