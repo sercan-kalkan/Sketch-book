6110cc35731ad6bcad117074e5ade8681d9cbb03
Sercan Kalkan <sercankalkan1@gmail.com>
2023-06-20 14:32:28 +0300

    all problems solved

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-//changing background color with mouse clicks
-/*
-let newColor = document.querySelectorAll(".row");
-          for (const color of newColor) {
-            color.addEventListener("mouseover",() => {
-                color.style.background = 'black';
-            });
-          }
-*/
-//create user the number of squares for both sides
-
+ 
 const button = document.getElementById('create');
 button.addEventListener('click',askSquareNumber);
-button.addEventListener('click',createSketch);
 
 let columns;
 let rows;
 
+//creating default sketch area 16*16 squares
+const height = `${(640/16)}px`//define height of square divs
+for (let i = 0; i < 16; ++i) {
+    let column = document.createElement('div'); // create column
+    column.classList.add('column');
+    column.style.setProperty("height", height)
+    rows = columns;
+    for (let j = 0; j < 16; ++j) {
+        let row = document.createElement('div'); // create row
+        row.classList.add('row');
+        row.style.setProperty("height", height)
+        column.appendChild(row); // append row in column
+      
+    }
+    let sketch= document.getElementById('sketch');
+    sketch.appendChild(column); // append column inside grid
+    painting()
+  }
 
+
+
+//asking the user square numbers
 function askSquareNumber() {
     columns = Number(prompt("How many squares do you want?"));
     console.log(typeof(columns));
@@ -25,11 +35,10 @@ function askSquareNumber() {
  }
 const freeSpace = document.getElementById('sketch');
 freeSpace.innerHTML=""
-sketch.inner
 createSketch(columns, rows);
 }
 
-
+//get the square number from user and create the sketch area
 function createSketch (columns, rows) {
     const height = `${(640/columns)}px`//define height of square divs
     for (let i = 0; i < columns; ++i) {
@@ -46,39 +55,19 @@ function createSketch (columns, rows) {
         }
         let sketch= document.getElementById('sketch');
         sketch.appendChild(column); // append column inside grid
+        painting()
       }
 }
-/*
 
-let columns =25;
-let rows =25;
-
-const height = `${(640/rows)}px`//define height of square divs
-
-for (let i = 0; i < columns; ++i) {
-  let column = document.createElement('div'); // create column
-  column.classList.add('column');
-  column.style.setProperty("height", height)
-
-  for (let j = 0; j < rows; ++j) {
-      let row = document.createElement('div'); // create row
-      row.classList.add('row');
-      row.style.setProperty("height", height)
-      column.appendChild(row); // append row in column
-    
-  }
-  let sketch= document.getElementById('sketch');
-  sketch.appendChild(column); // append column inside grid
+//painting the divs in the sketch book when mouse over
+function painting () {
+    let color = document.querySelectorAll('.row');
+    for(let element of color) {
+    element.addEventListener('mouseover',() => {
+        element.style.background='red';
+    });
 }
-*/
+console.log("painting")
+}
 
-//adding squares to sketch
-/*
-     for(let j=10; j<16; j++) {
-        for(let i=10; i<squareNumber; i++) {
-            const container = document.getElementById('container');
-            const div = document.createElement('div');
-            container.appendChild(div).classList.add('row');
-        }
-    }
-}*/
+
